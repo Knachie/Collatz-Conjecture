1da047667fcc323d1e14497649fcb60ef29bf45a
Kachi Ugwoke <kchugw@gmail.com>
2024-02-27 10:26:51 +0100

    Implemented the collatz algorithm

--- Collatz Conjecture/Program.cs
+++ Collatz Conjecture/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(Collatz(27));
+        }
+
+        private static int Collatz(int startNum)
+        {
+            int result = 0;
+            int colNum = startNum;
+
+            for (int i = 0; colNum != 1; i++)
+            {
+                if(colNum % 2 == 0)
+                {
+                    colNum /= 2;
+                }
+                else
+                {
+                    colNum = colNum * 3 + 1;
+                }
+                result++;
+            }
+
+            return result;
         }
     }
 }
